d9065d615e2b64fb60b2acac8d8ac5e2abd27869
MUsmanAliWafa <musmanaliwafa@gmail.com>
2023-08-22 10:34:13 +0700

    masukan form

--- js/script.js
+++ js/script.js
@@ -65,3 +65,21 @@ if (!localStorage.getItem('opened')) {
 }
 
 disableScroll();
+
+
+// form
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Konfirmasi kehadiran berhasil terkirim!");
+    })
+  });
+});
